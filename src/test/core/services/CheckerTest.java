a3536cf4da09320fdc074b92106e60cc36ae45ca
Yijie Wang <cremesponge@gmail.com>
2016-11-30 14:45:35 +0100

    Test Checker;Test DataCompute

---
+++ src/test/core/services/CheckerTest.java
@@ -0,0 +1,57 @@
+package test.core.services;
+
+import brainfuck.core.services.Checker;
+import brainfuck.exceptions.SyntaxErrorException;
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by user on 29/11/2016.
+ */
+public class CheckerTest {
+    Checker checker;
+
+    @Test
+    public void notWellFormed1() throws Exception {
+        String nwF1="++[+++[+-[++[--]--++]+]";
+        checker=new Checker(nwF1);
+        try {
+            checker.verify();
+            checker.isWellFormed();
+        }catch (SyntaxErrorException syn){
+            assertTrue(syn.getErrorCode()==4);
+        }
+    }
+
+    @Test
+    public void notWellFormed2() throws Exception {
+        String nwF2="++[++]-]++[++]";
+        checker=new Checker(nwF2);
+        try {
+            checker.verify();
+            checker.isWellFormed();
+        }catch (SyntaxErrorException syn){
+            assertTrue(syn.getErrorCode()==4);
+        }
+    }
+    /*
+        @Test
+        public void wellFormed1()throws Exception{
+            String wF1=null;
+            checker=new Checker(wF1);
+            checker.verify();
+            assertTrue(checker.isWellFormed());
+        }
+    */
+    @Test
+    public void wellFormed2()throws Exception{
+        String wF2="++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+        checker=new Checker(wF2);
+        checker.verify();
+        assertTrue(checker.isWellFormed());
+    }
+
+}
---
+++ src/test/core/services/DataComputeTest.java
@@ -0,0 +1,63 @@
+package test.core.services;
+
+import brainfuck.core.services.DataCompute;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by user on 30/11/2016.
+ */
+public class DataComputeTest {
+    @Before
+    public void setUp(){
+      
+    }
+
+    @Test
+    public void getGrille() throws Exception {
+
+    }
+
+    @Test
+    public void getPointerPos() throws Exception {
+
+    }
+
+    @Test
+    public void getPointedValue() throws Exception {
+
+    }
+
+    @Test
+    public void increment() throws Exception {
+
+    }
+
+    @Test
+    public void decrement() throws Exception {
+
+    }
+
+    @Test
+    public void pointerRight() throws Exception {
+
+    }
+
+    @Test
+    public void pointerLeft() throws Exception {
+
+    }
+
+    @Test
+    public void output() throws Exception {
+
+    }
+
+    @Test
+    public void input() throws Exception {
+
+    }
+
+}
